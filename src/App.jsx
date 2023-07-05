f243a8ce078de540c6e9cfe667c81ccd94cadab7
KevinDarnet <kevindarnet.work@gmail.com>
2023-07-05 11:47:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-2xl">Soy el body</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div>
+      <h1 className="text-red-600 text-3xl">Nav de kevin</h1>
+      <a className="text-1xl" href="">
+        Link
+      </a>
+    </div>
+  );
+}
